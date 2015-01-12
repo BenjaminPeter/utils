4d5d57942e520323b70d5bedf41cb855eaa15eb6
Benjamin Peter <bp@berkeley.edu>
2015-01-12 12:25:25 -0600

    initial commit

---
+++ plink.py
@@ -0,0 +1,27 @@
+import os
+import logging 
+
+
+def run_plink(plink, flags):
+    """runs plink. Parameters are handled with the flags command
+
+
+    
+    Parameters
+    ----------
+    plink : path
+        Path to a plink 1.9 executable
+    flags : dict
+        A dictionary, that contains the arguments for plink. an entry
+        flags[k] => v results in "--k v"
+    
+    """
+    
+    s = "%s " % plink
+    for item in flags.iteritems():
+        s += "--%s %s " % item
+
+    # s += " >>plink.log"
+
+    os.system(s)
+    logging.debug("ran plink with the following command\n%s" % s)
